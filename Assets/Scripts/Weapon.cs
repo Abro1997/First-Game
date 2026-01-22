b393064fa0e60d553f215b5c2cc96f864fd326e9
Abdellah Mouak <abdollah007mouak@gmail.com>
2026-01-22 21:21:04 +0100

    Weapon Fires BOOOLEEETS

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -3,14 +3,38 @@ using UnityEngine.InputSystem;
 
 public class Weapon : MonoBehaviour
 {
-    private void Update()
+
+    [SerializeField] private Transform firePoint;
+    [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private float fireRate = 0.5f;
+
+    private float lastFireTime;
+    private void Aim()
     {
         Vector2 mouseScreenPosition = Mouse.current.position.ReadValue();
-        Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(new Vector3(mouseScreenPosition.x, mouseScreenPosition.y, Camera.main.nearClipPlane));
-        Vector2 direction = mouseWorldPosition - transform.position;
+        Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(mouseScreenPosition);
+        mouseWorldPosition.z = 0f;
 
+        Vector2 direction = mouseWorldPosition - transform.position;
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 
-        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+        transform.rotation = Quaternion.Euler(0f, 0f, angle);
+    }
+
+    private void TryShoot()
+    {
+        if (Time.time < lastFireTime + fireRate) return;
+        
+        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        lastFireTime = Time.time;
+    }
+    private void Update()
+    {
+        Aim();
+
+        if (Mouse.current.leftButton.isPressed)
+        {
+            TryShoot();
+        }
     }
 }
